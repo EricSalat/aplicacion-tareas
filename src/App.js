48b716bc0757cc8217d7fd9877693d35ead91289
EricSalat <ericsalat@protonmail.com>
2022-10-18 20:56:21 +0200

    Version buildeada

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import LogoFreeCodeCamp from './componentes/LogoFreeCodeCamp';
 import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
     <div className="aplicacion-tareas">
-      <LogoFreeCodeCamp />
       <div className="tareas-lista-principal">
       <h1>Mis Tareas</h1>
-      <ListaDeTareas />
-      
+      <ListaDeTareas />    
     </div>
     </div>
   );
--- src/componentes/TareaFormulario.jsx
+++ src/componentes/TareaFormulario.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
-import "../hojas-de-estilo/TareaFormulario.css";
+import React, { Fragment, useState } from "react";
+import ReactDOM from "react-dom";
+import { useLocalStorage } from "../useLocalStorage";
 import { v4 as uuidv4 } from "uuid"
+import "../hojas-de-estilo/TareaFormulario.css";
 
 function TareaFormulario(props) {
   
   const [input, setInput] = useState("");
+
   
   const manejarCambio = e => {
     setInput(e.target.value)
@@ -14,21 +17,26 @@ function TareaFormulario(props) {
     
     //Para que el formulario no vuelva a cargar la página al enviarlo:
     e.preventDefault();
-
+    
     const tareaNueva = {
       id: uuidv4(),
       texto: input,
       completada: false
     };
-
+    
     //Que se pase la tarea Nueva cuando se active el prop OnSubmit
     props.onSubmit(tareaNueva);
   };
 
+  //Configuramos el LocalStorage
+  
+  const [text, setText] = useLocalStorage("text", "")
   
   return(
         <form 
           className="tarea-formulario"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           onSubmit={manejarEnvio}>
             <input
               className="tarea-input"
